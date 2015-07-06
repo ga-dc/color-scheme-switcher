99bda43c44860a47d3425a867199e3c2870e16e9
Matt Ficke <matthew.ficke@gmail.com>
2015-07-05 21:08:31 -0400

    loop though ids, run changeColor function each time

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ console.log(colorButton[0].id);
 
 for (i=0; i<colorButton.length; i++) {
   console.log(colorButton[i].id);
-//  changeColor(colorButton[i].id);
+  var color = colorButton[i].id;
+  console.log(color);
+  changeColor(color);
+  document.getElementById(color).addEventListener("click", function() {
+    changeColor(color);
+  });
+
 }
 
-//function changeColor(color) {
+function changeColor(color) {
+  document.getElementById(color).addEventListener("click", function() {
+    document.querySelector("body").style.backgroundColor = color.substring(0, color.length - 6);
+  });
+}
