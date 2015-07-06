ccf31144379a3176618c10789b792e0361259d03
Matt Ficke <matthew.ficke@gmail.com>
2015-07-05 20:33:04 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var colorButton = document.getElementById("switcher").querySelectorAll("li");
+console.log(colorButton[0].id);
+
+for (i=0; i<colorButton.length; i++) {
+  console.log(colorButton[i].id);
+//  changeColor(colorButton[i].id);
+}
+
+//function changeColor(color) {
